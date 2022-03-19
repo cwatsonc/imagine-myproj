9844fe70a0cef35c4649e4b87884e304dd5aa567
Charles Watson <charles.watson@acm.org>
2022-03-19 10:37:03 -0700

    fixing navigation and intra-app notification

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
 import App from './App';
 
-const mount = (el) => {
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+
     ReactDOM.render(
-        <App />, el
+        <App history={history} />, el
     );
+
+    return {
+        onContainerNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            console.log(nextPathname);
+            console.log(pathname);
+            if (nextPathname !== pathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 
 };
 
@@ -14,7 +34,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
